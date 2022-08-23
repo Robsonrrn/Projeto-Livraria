4cc0f8850749234710873743a4ed821962fbe8b3
Robson Nascimento <robsonnascimento@MacBook-Pro-de-Robson.local>
2022-08-23 10:58:16 -0300

    first commit

---
+++ Livraria/Controllers/LivrosController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Livraria.Models.DAL;
+using Livraria.Models.DAL.Repositorios;
+using Livraria.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livraria.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LivrosController : ControllerBase
+    {
+        private readonly ILivroRepositorio _livroRepositorio;
+
+        public LivrosController(ILivroRepositorio livroRepositorio)
+        {
+            _livroRepositorio = livroRepositorio;
+        }
+
+        [HttpGet]
+        public IEnumerable<LivroDTO> Get()
+        {
+            return _livroRepositorio.Get().Select(x => LivroDTO.ToDto(x)).OrderBy(x => x.Nome);
+        }
+
+        [HttpGet("{id}")]
+        public LivroDTO Get(int id)
+        {
+            var livro = _livroRepositorio.Get(id);
+
+            return LivroDTO.ToDto(livro);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] LivroDTO value)
+        {
+            _livroRepositorio.Add(new Livro
+            {
+                LivroNome = value.Nome,
+                LivroAutor = value.Autor,
+                LivroEditora = value.Editora,
+                LivroAnoPublicacao = value.AnoPublicacao
+            });
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] LivroDTO value)
+        {
+            var livro = _livroRepositorio.Get(id);
+            if (livro != null)
+            {
+                livro.LivroNome = value.Nome;
+                livro.LivroAutor = value.Autor;
+                livro.LivroEditora = value.Editora;
+                livro.LivroAnoPublicacao = value.AnoPublicacao;
+
+                _livroRepositorio.Update(livro);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _livroRepositorio.Delete(id);
+        }
+    }
+}
---
+++ Livraria/Models/DTOs/LivroDTO.cs
@@ -0,0 +1,25 @@
+﻿using Livraria.Models.DAL;
+
+namespace Livraria.Models.DTOs
+{
+    public class LivroDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public int AnoPublicacao { get; set; }
+
+        public static LivroDTO ToDto(Livro livro)
+        {
+            return new LivroDTO
+            {
+                Id = livro.LivroId,
+                Nome = livro.LivroNome,
+                Autor = livro.LivroAutor,
+                Editora = livro.LivroEditora,
+                AnoPublicacao = livro.LivroAnoPublicacao
+            };
+        }
+    }
+}
